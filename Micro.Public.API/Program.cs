ab23d7de39c2f4364981b8e475fc2ad69219ffd9
512085580@qq.com <512085580@qq.com>
2022-04-17 16:37:51 +0800

    0.0

---
+++ Micro.Public.API/Program.cs
@@ -0,0 +1,35 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+#if !DEBUG
+//Ö¸¶¨±©Â¶¶Ë¿Ú
+builder.WebHost.ConfigureKestrel((context, serverOptions) =>
+{
+    serverOptions.Listen(IPAddress.Loopback, 80);
+    //serverOptions.Listen(IPAddress.Loopback, 5001, listenOptions =>
+    //{
+    //    listenOptions.UseHttps("testCert.pfx", "testPassword");
+    //});
+});
+#endif
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
