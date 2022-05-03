2c05ac0eeff3b8aa0f2d8b694dd02f132531a93d
512085580@qq.com <512085580@qq.com>
2022-05-03 17:01:45 +0800

    0.0

--- Micro.Hotel.API/Program.cs
+++ Micro.Hotel.API/Program.cs
@@ -1,25 +1,41 @@
+using Microsoft.AspNetCore.Hosting;
 using System.Net;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-#if !DEBUG
-//指定暴露端口
-builder.WebHost.ConfigureKestrel((context, serverOptions) =>
-{
 
-    //监听端口 80 
-    serverOptions.ListenLocalhost(5005);
 
-    //serverOptions.Listen(IPAddress.Loopback, 80);
-    //serverOptions.Listen(IPAddress.Loopback, 80, listenOptions =>
-    //{
-    //    listenOptions.UseHttps("testCert.pfx", "testPassword");
-    //});
-});
+//使用WebHost
+builder.WebHost.UseKestrel(webhost =>
+    {
+        //实现对特定端口的监控
+        webhost.ListenAnyIP(5005);
 
-#endif
+        //↓↓↓↓↓↓↓↓↓↓ 各种资源限制↓↓↓↓↓↓↓↓↓↓
+
+        //最大并发连接。如果设置为Null（默认值），意味着不作限制。
+        webhost.Limits.MaxConcurrentConnections = 500;
+
+        //可升级连接（比如从HTTP升级到WebSocket）的最大并发数。如果设置为Null（默认值），意味着不作限制。
+        webhost.Limits.MaxConcurrentUpgradedConnections = 500;
+
+        //请求主体最大字节数，默认值为30,000,000 字节（约28.6M）。如果设置为Null，意味着不作限制。
+        webhost.Limits.MaxRequestBodySize = (1024 * 100); //修改成 100kb
+
+        //接收请求报头的超时时间，默认为30秒。
+        webhost.Limits.RequestHeadersTimeout = TimeSpan.FromSeconds(35);  //修改成 35秒
+
+        // 响应缓冲区最大容量，默认值为65,536（64kb）。  
+        webhost.Limits.MaxResponseBufferSize = (1024 * 100);  //修改成 100kb
+
+
+        // 其他各种限制  . . . . . . . .
+
+    });
+
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -31,8 +47,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 //if (app.Environment.IsDevelopment())
 //{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+app.UseSwagger();
+app.UseSwaggerUI();
 //}
 
 app.UseAuthorization();
--- Micro.Order.API/Program.cs
+++ Micro.Order.API/Program.cs
@@ -4,20 +4,34 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-#if !DEBUG
-//指定暴露端口
-builder.WebHost.ConfigureKestrel((context, serverOptions) =>
+//使用WebHost
+builder.WebHost.UseKestrel(webhost =>
 {
+    //实现对特定端口的监控
+    webhost.ListenAnyIP(5005);
 
-    serverOptions.ListenLocalhost(5006);
+    //↓↓↓↓↓↓↓↓↓↓ 各种资源限制↓↓↓↓↓↓↓↓↓↓
 
-    //serverOptions.Listen(IPAddress.Loopback, 80);
-    //serverOptions.Listen(IPAddress.Loopback, 5001, listenOptions =>
-    //{
-    //    listenOptions.UseHttps("testCert.pfx", "testPassword");
-    //});
+    //最大并发连接。如果设置为Null（默认值），意味着不作限制。
+    webhost.Limits.MaxConcurrentConnections = 500;
+
+    //可升级连接（比如从HTTP升级到WebSocket）的最大并发数。如果设置为Null（默认值），意味着不作限制。
+    webhost.Limits.MaxConcurrentUpgradedConnections = 500;
+
+    //请求主体最大字节数，默认值为30,000,000 字节（约28.6M）。如果设置为Null，意味着不作限制。
+    webhost.Limits.MaxRequestBodySize = (1024 * 100); //修改成 100kb
+
+    //接收请求报头的超时时间，默认为30秒。
+    webhost.Limits.RequestHeadersTimeout = TimeSpan.FromSeconds(35);  //修改成 35秒
+
+    // 响应缓冲区最大容量，默认值为65,536（64kb）。  
+    webhost.Limits.MaxResponseBufferSize = (1024 * 100);  //修改成 100kb
+
+
+    // 其他各种限制  . . . . . . . .
+
 });
-#endif
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
