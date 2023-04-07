9d69469f708ada9c5d34e91658f69c633b3627b1
yxdeng <yxdeng@homeinns.com>
2023-04-07 18:02:12 +0800

    -

---
+++ Micro.Order.API/DynamicApi/FurionAppService.cs
@@ -0,0 +1,16 @@
+﻿namespace Micro.Order.API.DynamicApi
+{
+
+
+	/// <summary>
+	///  动态API
+	/// 继承 IDynamicApiController 接口 或 贴 [DynamicApiController] 特性
+	/// </summary>
+	public class FurionAppService : IDynamicApiController
+	{
+		public string Get()
+		{
+			return $"Hello  Dynamic Api  {nameof(Furion)}";
+		}
+	}
+}
